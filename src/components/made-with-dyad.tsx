22378580cb473fcf27414e73a4a7990bc038b6db
M3RD-Nanda <m.3rd.nanda@gmail.com>
2025-07-07 17:18:23 +0700

    tes

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,7 +2,7 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center">
       <a
-        href="https://www.dyad.sh/"
+        href="https://www.google.com/search?sxsrf=AE3TifNMXDMURgxmKXRh61sfEgoV_GfJzA:1751883484538&q=i+love+you&udm=2"
         target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
